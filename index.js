e87abeb17f91503c538969fe3a4e37f426a86c6a
rxd5mp0 <bdeckard92@gmail.com>
2021-10-11 15:20:49 -0500

    adds new urlParser and unified topology to mongoose connection

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const app = express();
 const PORT = 4000;
 
 // bodyparser setup
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/productsdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
 
