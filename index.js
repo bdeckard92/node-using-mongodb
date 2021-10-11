9c1e61c3aa16907cc0e8c3b6675a50ba99a69a60
rxd5mp0 <bdeckard92@gmail.com>
2021-10-11 15:14:17 -0500

    installs mongoose and sets connection

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
-);
+app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
